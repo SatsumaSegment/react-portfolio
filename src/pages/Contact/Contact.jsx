1d52b64b35c53456eb88ba079476fa144a21eb6f
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-19 13:24:25 +0000

    Added a functioning contact form that will email me when someone sends a message

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
 import "./Contact.css";
 
 function Contact() {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [stateMessage, setStateMessage] = useState(null);
+    const sendEmail = (e) => {
+        e.persist();
+        e.preventDefault();
+        setIsSubmitting(true);
+        emailjs.sendForm("service_bhqx3qy", "template_rojsk3j", e.target, "G9Gh7gZqPRSLA7U8y").then(
+            (result) => {
+                setStateMessage("Message sent!");
+                setIsSubmitting(false);
+                setTimeout(() => {
+                    setStateMessage(null);
+                }, 5000); // hide message after 5 seconds
+            },
+            (error) => {
+                setStateMessage("Something went wrong, please try again later");
+                setIsSubmitting(false);
+                setTimeout(() => {
+                    setStateMessage(null);
+                }, 5000); // hide message after 5 seconds
+            }
+        );
+
+        // Clears the form after sending the email
+        e.target.reset();
+    };
+
     return (
         <div className="contact-page">
             <h1 className="contact-title">Contact Me</h1>
@@ -8,20 +38,21 @@ function Contact() {
             <div className="contact-main">
                 <div className="socials">socials</div>
                 <div className="contact-form-container">
-                    <form className="contact-form" method="POST">
+                    <form className="contact-form" onSubmit={sendEmail}>
                         <div className="group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" className="form-input" />
+                            <label htmlFor="from_name">Name</label>
+                            <input type="text" name="from_name" id="from_name" className="form-input" />
                         </div>
                         <div className="group">
                             <label htmlFor="email">Email Address</label>
-                            <input type="email" aria-describedby="emailHelp" className="form-input" />
+                            <input type="email" aria-describedby="emailHelp" name="email" id="email" className="form-input" />
                         </div>
                         <div className="group">
                             <label htmlFor="message">Message</label>
-                            <textarea className="form-input" rows={10}></textarea>
+                            <textarea className="form-input" name="message" id="message" rows={10}></textarea>
                         </div>
-                        <button className="form-button">Send</button>
+                        <input className="contact-button" type="submit" value="Send" disabled={isSubmitting} />
+                        {stateMessage && <p>{stateMessage}</p>}
                     </form>
                 </div>
             </div>
